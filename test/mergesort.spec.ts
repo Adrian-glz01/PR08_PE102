1f063d14e545b7dcfee4cc0763b44b95d8b28913
Adrian Gonzalez <alu0101404813@ull.edu.es>
2023-03-20 10:53:47 +0000

    PR8

--- test/mergesort.spec.ts
+++ test/mergesort.spec.ts
@@ -8,6 +8,8 @@ describe('MergeSort', () => {
   const array = [9,8,7,6,5,4,3,2,1];
   const array2 = [10,9,8,7,6,5,4,3,2,1];
   const array3 = [2,4,6,8,9,1,3,5,7,10];
+  const array4 = [2,4,6,8,9,1,3,5,7,10];
+
 
   
   it('should return [1,2,3,4,5,6,7,8,9]', () => {
@@ -26,7 +28,7 @@ describe('MergeSort', () => {
     expect(solver.getData()).to.be.eql([1,2,3,4,5,6,7,8,9,10]);
   });
   it ('should return [1,2,3,4,5,6,7,8,9,10]', () => {
-    const solver = new Solver(array3, new MergeSort());
+    const solver = new Solver(array4, new MergeSort());
     solver.setStrategy(new BubbleSort());
     solver.logic();
     expect(solver.getData()).to.be.eql([1,2,3,4,5,6,7,8,9,10]);
